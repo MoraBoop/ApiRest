0bf463373c0663179e0366f9557398c9bb177828
Jason <jasonmoramora@outlook.com>
2019-02-28 23:25:03 -0500

    Primera version stable

--- index.js
+++ index.js
@@ -4,29 +4,24 @@ const morgan = require('morgan');
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
+
+
+var app = express();
+var port = process.env.PORT || 3000; 
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 // Connection URL
 const url = 'mongodb://localhost:27017';
-
 // Database Name
 const dbName = 'almacen';
-
 // Use connect method to connect to the server
 MongoClient.connect(url, (err, client)=> {
     assert.equal(null, err);
     console.log("Connected successfully to server");
-
     const db = client.db(dbName);
-
     client.close();
 });
-
-var app = express();
-var port = process.env.PORT || 3000; 
-
-app.use(express.static('public'));
-app.use(morgan('dev'));
-
-
 
 app.get('/', (req, res)=>{
     res.send('Home page');
@@ -39,17 +34,20 @@ const findDocuments = (db, callback)=>{
         console.log(docs)
         callback(docs);
     });
-}
+};
 
 app.get('/articulos', (req, res)=>{
     MongoClient.connect(url, (err, client)=>{
         assert.equal(null, err);
         const db = client.db(dbName);
-        findDocuments(db, ()=>{
+        findDocuments(db, (result)=>{
+            res.send(result);
             client.close();
         });
     });
 });
+
+
 
 app.listen(port, ()=>{
     console.log('App run port 3000');
